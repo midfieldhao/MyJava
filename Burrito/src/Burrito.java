93daef962f7d1892b24c63e3dc0219570fb65c0b
midfieldhao <Midfield@163.com>
2023-02-08 17:15:43 +0800

    2023-02-08 17:15

--- Burrito/src/Burrito.java
+++ Burrito/src/Burrito.java
@@ -8,9 +8,10 @@ public class Burrito {
     Spiciness degree;
     public  Burrito(Spiciness degree) { this.degree = degree;}
     public void describe() {
-        System.out.print("This burrito is");
+        System.out.print("This burrito is ");
         switch(degree) {
             case NOT:    System.out.println("not spicy at all.");
+                         break;
             case MILD:
             case MEDIUM: System.out.println("a little hot.");
                          break;
