4ca1013eb629b98818518fc92fddf4075d6f02fa
midfieldhao <Midfield@163.com>
2023-03-06 17:26:13 +0800

    2023-03-06 17:26

---
+++ InnerClasses/src/GreenhouseController.java
@@ -0,0 +1,26 @@
+//: innerclasses/GreenhouseController.java
+// Configure and execute the greenhouse system.
+// {Args: 5000}
+
+import controller.*;
+
+public class GreenhouseController {
+    public static void main(String[] args) {
+        GreenhouseControls gc = new GreenhouseControls();
+        // Instead of hard-wiring, you clould parse
+        // configuration information from a text file here:
+        gc.addEvent(gc.new Bell(900)); 
+        Event[] eventList = {
+            gc.new ThermostatNight(0),
+            gc.new LightOn(200),
+            gc.new LightOff(400),
+            gc.new WaterOn(600),
+            gc.new WaterOff(800),
+            gc.new ThermostatDay(1400)
+        };
+        gc.addEvent(gc.new Restart(2000, eventList));
+        if (args.length == 1)
+          gc.addEvent(new GreenhouseControls.Terminate(Long.valueOf(args[0])));
+        gc.run();
+    }
+}
